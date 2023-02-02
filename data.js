7c1efb5673ee04665cbd35a77f3ffa987e8d9d63
Hammadulhaq123 <professorcoding123@gmail.com>
2023-02-03 02:01:01 +0500

    First Commit

---
+++ data.js
@@ -0,0 +1,56 @@
+let songs = [
+    {
+        name:'Pathan',
+        path:'Assets/Tracks/jhoomepathan.mp3',
+        artist: 'Vishal Shekhar',
+        cover: 'Assets/Covers/scg/jhoomepathan.jpg'
+    },
+    {
+        name:'Munda hoon',
+        path:'Assets/Tracks/chedkhaniyan.mpeg',
+        artist: 'Diljit',
+        cover: 'Assets/Covers/scg/chedkhaniyan.jpg'
+    },
+    {
+        name:'Maverick',
+        path:'Assets/Tracks/Top Gun Maverick.mp3',
+        artist: 'One Republic',
+        cover: 'Assets/Covers/scg/maverick.jpg'
+    },
+    {
+        name:'Bones',
+        path:'Assets/Tracks/bones.mpeg',
+        artist: 'Imagine Dragons',
+        cover: 'Assets/Covers/scg/bones.jpeg'
+    },
+    {
+        name:'Kurulus',
+        path:'Assets/Tracks/kurulus.mpeg',
+        artist: 'unknown',
+        cover: 'Assets/Covers/scg/kurulus.jpeg'
+    },
+    {
+        name:'Kesariya',
+        path:'Assets/Tracks/kesariya.mpeg',
+        artist: 'Arijit Singh',
+        cover: 'Assets/Covers/scg/keariya.jpg'
+    },
+    {
+        name:'Pasoori',
+        path:'Assets/Tracks/pasoori.mpeg',
+        artist: 'Coke Studio',
+        cover: 'Assets/Covers/scg/pasoori.jpeg'
+    },
+    {
+        name:'Jeda Nasha',
+        path:'Assets/Tracks/nasha.mpeg',
+        artist: 'Amar Jalal',
+        cover: 'Assets/Covers/scg/nasha.jpeg'
+    },
+    {
+        name:'Tu Mera',
+        path:'Assets/Tracks/Tu Mera.mpeg',
+        artist: 'Amar Jalal',
+        cover: 'Assets/Covers/categories/download.jpeg'
+    }
+]
